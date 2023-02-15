c3c18abb549cea9f5fe429efdb584b4cefb43f3e
Leidy Arango <leidy.arango@evertecinc.com>
2023-02-14 23:55:51 -0500

    feat: add fibonacci while

--- app/Http/Controllers/FibonacciController.php
+++ app/Http/Controllers/FibonacciController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RequestNum;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
 class FibonacciController extends Controller
@@ -22,6 +21,27 @@ class FibonacciController extends Controller
 
         for($i = 2; $i < $num; $i++) {
             $fibonacci[] = $fibonacci[$i -1] + $fibonacci[$i -2];
+        }
+
+        return view('fibonacci.show', ['fibonacci' => $fibonacci, 'num' => $num]);
+    }
+
+    public function otherOption(RequestNum $request): View
+    {
+        $fibonacci = [];
+
+        $num = $request->input('num');
+
+        $num1 = 0;
+        $num2 = 1;
+
+        $counter = 0;
+        while ($counter < $num){
+            $fibonacci[] = $num1;
+            $num3 = $num2 + $num1;
+            $num1 = $num2;
+            $num2 = $num3;
+            $counter = $counter + 1;
         }
 
         return view('fibonacci.show', ['fibonacci' => $fibonacci, 'num' => $num]);
